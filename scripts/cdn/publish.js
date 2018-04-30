55dd9b0db458ffa18db6c3ae38ea953cb92bc438
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-04-30 17:56:28 +0200

    Add CDN release scripts

---
+++ scripts/cdn/publish.js
@@ -0,0 +1,56 @@
+/**
+ * Publish packages to our CDN
+ */
+
+// Load secrets file
+
+var fs = require('fs');
+
+var secrets = JSON.parse(fs.readFileSync('secrets.json'));
+if (!secrets ||
+    !secrets.AWS_USER_S3_KEY ||
+    !secrets.AWS_USER_S3_SECRET ||
+    !secrets.AWS_S3_BUCKET) {
+    throw Error('secrets.json content is not valid');
+}
+
+// Load package version
+
+var semver = require('semver');
+
+var version = JSON.parse(fs.readFileSync('package.json')).version;
+if (!version || !semver.valid(version)) {
+    throw Error('package.json version is not valid');
+}
+
+// Publish to CDN
+
+var s3 = require('s3');
+
+var client = s3.createClient({
+    s3Options: {
+        accessKeyId: secrets.AWS_USER_S3_KEY,
+        secretAccessKey: secrets.AWS_USER_S3_SECRET
+    }
+});
+
+uploadFiles(version);
+
+function uploadFiles(version) {
+    console.log('Publish', version);
+    var uploader = client.uploadDir({
+        localDir: 'dist',
+        s3Params: {
+            Bucket: secrets.AWS_S3_BUCKET,
+            Prefix: 'mapbox-gl/' + version + '/'
+        }
+    });
+    uploader.on('error', function(err) {
+        console.error('Error: unable to upload:', err.stack);
+    });
+    uploader.on('progress', function() {
+    });
+    uploader.on('end', function() {
+        console.log('Done', version);
+    });
+}
---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VERSION=$(node --eval "console.log(require('./package.json').version);")
+
+echo "Ready to publish MGL version $VERSION"
+echo "Has the version number been bumped?"
+read -n1 -r -p "Press Ctrl+C to cancel, or any other key to continue." key
+
+echo "Uploading to CDN..."
+
+node scripts/cdn/publish.js
+node scripts/cdn/invalidate.js
+
+# echo "Uploading to npm..."
+
+# npm publish
+
+echo "All done."
