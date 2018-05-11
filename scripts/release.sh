427fe96805a545506d4230afa22fc61e36e9fba2
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-05-11 15:48:42 +0200

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,13 +6,13 @@ echo "Ready to publish MGL version $VERSION"
 echo "Has the version number been bumped?"
 read -n1 -r -p "Press Ctrl+C to cancel, or any other key to continue." key
 
+echo "Uploading to npm..."
+
+npm publish
+
 echo "Uploading to CDN..."
 
 node scripts/cdn/publish.js
 node scripts/cdn/invalidate.js
-
-# echo "Uploading to npm..."
-
-# npm publish
 
 echo "All done."
